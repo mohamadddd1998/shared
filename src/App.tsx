59e9f21e16ebe93a34b381e00a90eb06dfab4f81
mohamadddd1998 <mohamadmirzaei1998@gmail.com>
2025-02-20 15:10:02 +0330

    f

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ function App() {
 
   return (
     <>
-    <a></a>
-    <p>تست</p>
-    <button>تست</button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
