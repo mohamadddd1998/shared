23d4d59a1206f5588ad486ee2065dcb3e04c71fc
mohamadddd1998 <mohamadmirzaei1998@gmail.com>
2025-02-21 00:18:05 +0330

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button'
-
+import Alert from './components/Alert'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Button />
+      <Alert />
     </>
   )
 }
-
 export default App
